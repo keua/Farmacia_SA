170d38503deaa13f60db32848fe363b0b48986c8
TAmyG <tamy.vivas@gmail.com>
2015-12-28 13:18:54 -0600

    archivo dump mysql

---
+++ dump.sql
@@ -0,0 +1,274 @@
+-- MySQL dump 10.13  Distrib 5.5.46, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: farmacia_db
+-- ------------------------------------------------------
+-- Server version	5.5.46-0ubuntu0.14.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `farmacia_db`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `farmacia_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `farmacia_db`;
+
+--
+-- Table structure for table `bill`
+--
+
+DROP TABLE IF EXISTS `bill`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bill` (
+  `mount` float NOT NULL,
+  `date` datetime DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `employee_id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bill`
+--
+
+LOCK TABLES `bill` WRITE;
+/*!40000 ALTER TABLE `bill` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bill` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bill_medicines`
+--
+
+DROP TABLE IF EXISTS `bill_medicines`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bill_medicines` (
+  `bill_id` int(11) NOT NULL,
+  `medicines_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `PriceUnit` float NOT NULL,
+  KEY `bill_id_index` (`bill_id`),
+  KEY `medicines_id_index` (`medicines_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bill_medicines`
+--
+
+LOCK TABLES `bill_medicines` WRITE;
+/*!40000 ALTER TABLE `bill_medicines` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bill_medicines` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `client` (
+  `name` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `nit` varchar(255) NOT NULL,
+  `phoneNumber` int(11) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `birth` date DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nit_unique` (`nit`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `client`
+--
+
+LOCK TABLES `client` WRITE;
+/*!40000 ALTER TABLE `client` DISABLE KEYS */;
+INSERT INTO `client` VALUES ('Juan','Lopez','12345678',12342123,'SMP','0000-00-00',1);
+/*!40000 ALTER TABLE `client` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `drugstore`
+--
+
+DROP TABLE IF EXISTS `drugstore`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `drugstore` (
+  `name` varchar(255) NOT NULL,
+  `phoneNumber` int(11) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `drugstore`
+--
+
+LOCK TABLES `drugstore` WRITE;
+/*!40000 ALTER TABLE `drugstore` DISABLE KEYS */;
+INSERT INTO `drugstore` VALUES ('atanacio',12345678,'calz 12-13 zona 12',1),('requena',12345678,'san miguel petapa',2),('carolina & h',12345678,'villa nueva 12-54 ave. helena',3);
+/*!40000 ALTER TABLE `drugstore` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `drugstore_medicines`
+--
+
+DROP TABLE IF EXISTS `drugstore_medicines`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `drugstore_medicines` (
+  `medicines_id` int(11) NOT NULL,
+  `drugstore_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `PriceUnit` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `drugstore_medicines`
+--
+
+LOCK TABLES `drugstore_medicines` WRITE;
+/*!40000 ALTER TABLE `drugstore_medicines` DISABLE KEYS */;
+INSERT INTO `drugstore_medicines` VALUES (1,2,107,10),(2,2,200,35),(3,2,255,15),(4,2,120,16);
+/*!40000 ALTER TABLE `drugstore_medicines` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employee` (
+  `name` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `phoneNumber` int(11) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `birth` date DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `drugstore_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES ('Juan','Miguel','201212123',12342123,'SMP','1234','0000-00-00',1,1),('Juan','Gabriel','201212124',12342123,'SMP','1234','0000-00-00',2,2),('Josue','Chacon Merica','201212125',12342666,'SMP','1234','0000-00-00',3,3),('Yo','Vivas','201212126',12342666,'SMP','1234','0000-00-00',4,3),('Gilberto','Cruz','201212127',12342123,'SMP','1234','2012-07-08',5,2);
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `medicine`
+--
+
+DROP TABLE IF EXISTS `medicine`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `medicine` (
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `id` float NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `medicine`
+--
+
+LOCK TABLES `medicine` WRITE;
+/*!40000 ALTER TABLE `medicine` DISABLE KEYS */;
+INSERT INTO `medicine` VALUES ('vitaflenaco','alivia el dolor',1),('aspirina','alivia el dolor de cabeza',2),('alka d','alivia la diarrea',3),('penicilina','alivia de todo',4),('penicilina','alivia de todo',5);
+/*!40000 ALTER TABLE `medicine` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `payment`
+--
+
+DROP TABLE IF EXISTS `payment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `payment` (
+  `mount` float NOT NULL,
+  `surcharge` float NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bill_id` int(11) NOT NULL,
+  `paymenttype_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `payment`
+--
+
+LOCK TABLES `payment` WRITE;
+/*!40000 ALTER TABLE `payment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `payment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `paymenttype`
+--
+
+DROP TABLE IF EXISTS `paymenttype`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `paymenttype` (
+  `name` varchar(255) NOT NULL,
+  `surcharge` float NOT NULL,
+  `id` float NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `paymenttype`
+--
+
+LOCK TABLES `paymenttype` WRITE;
+/*!40000 ALTER TABLE `paymenttype` DISABLE KEYS */;
+/*!40000 ALTER TABLE `paymenttype` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-12-28 13:17:49
