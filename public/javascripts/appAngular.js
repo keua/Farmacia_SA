1336217047dc62c98aea0e651a6dcc6780598b83
TAmyG <tamy.vivas@gmail.com>
2016-01-02 18:56:58 -0600

    test10

--- public/javascripts/appAngular.js
+++ public/javascripts/appAngular.js
@@ -1,6 +1,6 @@
-angular.module('appChaplist', ['ui.router', 'controllers', 'factories', 'datatables','LocalStorageModule'])
+angular.module('appChaplist', ['ui.router', 'controllers', 'factories', 'datatables', 'LocalStorageModule'])
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state('index', {
             url: '/index',
@@ -15,19 +15,13 @@ angular.module('appChaplist', ['ui.router', 'controllers', 'factories', 'datatab
         .state('admin', {
             url: '/admin',
             templateUrl: 'views/adminMedicine.html',
-<<<<<<< HEAD
-            controller: 'ctrlAdmin'
+            controller: 'ctrlAdmin as chowCaseAdmin'
         })
         .state('indexCC', {
             url: '/indexCC',
             templateUrl: 'views/indexCC.html',
             controller: 'ctrlHomeCC'
         })
-=======
-            controller: 'ctrlAdmin as showCaseAdmin'
-        });
->>>>>>> 191dc52f8ba1b7c7160ae3332fe8ef6c5b5937ae
-
         .state('HomeCC', {
             url: '/HomeCC',
             templateUrl: 'views/HomeCC.html',
